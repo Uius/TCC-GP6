9825e90e1c799533195e1d71c4374c2a93fdf03c
VictorMarquezini <victorhugogarciamarquezini600@gmail.com>
2025-08-08 11:38:25 +0000

    Login 99%

--- tccgp6/src/app/login/page.tsx
+++ tccgp6/src/app/login/page.tsx
@@ -4,21 +4,13 @@ import React from "react";
 export default function Login() {
   return (
     <div className="min-h-screen flex items-center justify-center bg-gradient-to-br from-gray-900 to-black">
-      <div className="h-175 bg-yellow-600 p-20 rounded-md shadow-lg bodyloguin">
-        <div className="flex flex-col items-center mb-6">
+      <div className="h-143 bg-yellow-600 p-20 rounded-md shadow-lg bodyloguin">
+        <div className="flex flex-col items-center mb-6 -mt-18">
           <Image
-<<<<<<< HEAD
             src="/LOGOPRETO.png"// você vai precisar colocar essa imagem na pasta public
             alt="Logo Leão"
-            width={350}
-            height={350}
-=======
-          className="-m-15 mb-1"
-            src="/LOGOPRETO.png" // você vai precisar colocar essa imagem na pasta public
-            alt="Logo Leão"
-            width={362}
-            height={150}
->>>>>>> 709d56321bf1743ea4883789c3ad434b4fdcd50f
+            width={280}
+            height={280}
           />
           
         </div>
@@ -29,25 +21,25 @@ export default function Login() {
             <input
               type="text"
               className="text-[#4B4B4B] rounded-[20] bg-sky-50 shadow-xl w-full p-2 border-white-600 focus:outline-none focus:ring-2 focus:ring-yellow-500"
-              placeholder="CNPJ: "
+              placeholder="  CNPJ: "
             />
           </div>
           <div>
             <label className="w-15 text-shadow-lg/30 font-bold block text-white mb-1">SENHA:</label>
             <input
               type="password"
-              className="text-[#4B4B4B] rounded-[20] bg-sky-50 shadow-xl w-full p-2 border-white-600 focus:outline-none focus:ring-2 focus:ring-yellow-500"
+              className="text-[#4B4B4B] absolute rounded-[20] bg-sky-50 shadow-xl w-full p-2 border-white-600 focus:outline-none focus:ring-2 focus:ring-yellow-500"
               placeholder="  Senha: "
             />
             <p  className="text-shadow-lg/30 block text-white mb-1 justify-self-end text-[15px] m-1 underline"> Esqueceu sua senha? </p>
           </div>
           <br></br>
-          <div className="flex justify-between mt-6">
+          <div className="flex justify-between mt-2">
             <button
               type="submit"
               className="font-bold rounded-[15] w-35 shadow-lg/30 bg-black text-white px-4 py-2 rounded hover:bg-gray-800 transition"
             >
-              Confirmar
+              Entrar
             </button>
             <button
               type="button"
