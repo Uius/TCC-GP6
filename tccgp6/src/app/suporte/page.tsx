3c43453a7848a1d4ad9338865bb9c9171e019e95
William <168018978+Uius@users.noreply.github.com>
2025-08-29 12:01:33 +0000

    suporte agr volta pra home

--- tccgp6/src/app/suporte/page.tsx
+++ tccgp6/src/app/suporte/page.tsx
@@ -24,7 +24,7 @@ export default function HomePage() {
 
         <div className="flex items-center gap-6">
           <nav className="flex gap-6 text-sm">
-            <a href="#" className="text-white py-2 px-4">HOME</a>
+            <a href="/" className="text-white py-2 px-4">HOME</a>
             <a href="/estoque" className="text-white py-2 px-4">ESTOQUE</a>
             <a href="/suporte" className="text-white bg-gray-800 py-2 px-4 underline rounded-sm">SUPORTE</a>
           </nav>
